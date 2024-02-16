11fbb5062668bd33f9094ed5115d1892c2fa5700
MJ Durandar <mjdurandarjr@gmail.com>
2024-02-16 18:41:21 +0800

    fixes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'address' => ['required', 'string', 'address', 'max:255'],
+            // 'address' => ['required', 'string', 'address', 'max:255'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
